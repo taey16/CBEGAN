dd7ee2ea8ed09c4cfa52408b42378634879629b1
taey16 <taey1600@gmail.com>
2017-06-13 12:33:42 +0900

    default cond_size 2

--- main_CBEGAN.py
+++ main_CBEGAN.py
@@ -34,7 +34,7 @@ parser.add_argument('--outputChannelSize', type=int,
 parser.add_argument('--ngf', type=int, default=128)
 parser.add_argument('--ndf', type=int, default=128)
 parser.add_argument('--hidden_size', type=int, default=64, help='bottleneck dimension of Discriminator')
-parser.add_argument('--cond_size', type=int, default=0,  help='Whether to use conditional GAN')
+parser.add_argument('--cond_size', type=int, default=2,  help='Whether to use conditional GAN')
 parser.add_argument('--niter', type=int, default=30, help='number of epochs to train for')
 parser.add_argument('--lrD', type=float, default=0.00005, help='learning rate')
 parser.add_argument('--lrG', type=float, default=0.00005, help='learning rate')
